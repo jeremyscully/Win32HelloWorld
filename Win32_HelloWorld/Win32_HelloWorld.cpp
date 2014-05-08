ad89805bf8f64c28629dfc42d588a41e22163f10
jeremyscully <jeremys@expedia.com>
2014-05-08 10:02:41 -0700

    Creating initial project for C++ Jenkins Build Testing

---
+++ Win32_HelloWorld/Win32_HelloWorld.cpp
@@ -0,0 +1,10 @@
+// Win32_HelloWorld.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+#include "stdafx.h"
+int main(){
+	std::cout << "Hello, World\n";
+	system("PAUSE");
+}
+
+
